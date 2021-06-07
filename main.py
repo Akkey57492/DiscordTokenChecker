e283e962eb0123b4dd2cbf33ec850d584d661e21
Akkey57492 <84906515+Akkey57492@users.noreply.github.com>
2021-06-08 06:28:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 import re
 
 print("""
@@ -16,7 +17,14 @@ for token in tokens:
     result = token.replace("\n", "")
     Headers = {"authorization": f"{result}"}
     Check = requests.get("https://discordapp.com/api/v6/auth/login", headers=Headers)
+    GetResponse = Check.json()
+    Failed = GetResponse["message"]
     if Check.status_code == 200:
         print(f"ValidToken: {result}")
+    elif Failed == "You are being rate limited.":
+        print(f"Failed: {result}")
+        print("一時的な制限がかかりました。")
+        print("120秒後(2分後)に再開します。")
+        time.sleep(120)
     else:
         print(f"InvalidToken: {result}")
